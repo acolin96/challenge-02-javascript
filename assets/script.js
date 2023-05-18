b3bfc1dbe474aba9ccd1248022cf81f19ccc14f7
Alex Colin <acolin3196@gmail.com>
2023-05-18 18:52:18 -0500

    added a readme

--- assets/script.js
+++ assets/script.js
@@ -10,6 +10,7 @@ function generatePassword() {
     numeric: "0123456789",
     special: "!@#$%^&*()_+-={}[]|\\:;\"',.?/~`"
   };
+  console.log(charSets)
 
   // Prompt user for password character length. Had to use google to find the parseInt but that is what ties the number value stored in the passlength.
   var passLength = parseInt(prompt("Enter password length (between 8 and 128 characters):"));
@@ -18,12 +19,19 @@ function generatePassword() {
     alert("Invalid password length!!! Please enter a number between 8 and 128.");
     return;
   }
-
+  console.log(passLength);
+  
   // Prompt user for character types to include in password. I feel like there is an easier way to do this but i dont know how.
+
   var useLowercase = confirm("Include lowercase letters?");
   var useUppercase = confirm("Include uppercase letters?");
   var useNumeric = confirm("Include numeric characters?");
   var useSpecial = confirm("Include special characters?");
+
+  console.log(useLowercase);
+  console.log(useUppercase);
+  console.log(useNumeric);
+  console.log(useSpecial);
 
   // Make sure at least one character type is selected. I'll be honest I googled this one. I struggle to understand the use of "!" before the variable name. What I think it does is display the alert if they dont choose any of these but still select a password length.
    if (!useLowercase && !useUppercase && !useNumeric && !useSpecial) {
@@ -31,7 +39,7 @@ function generatePassword() {
      return;
    }
 
-  // Build character set based on user selections
+  // Build character set based on user selections. was wondering if i could make else statements instead of 4 if statements, but i wasnt confident.
   var charSet = "";
   if (useLowercase) {
     charSet += charSets.lowercase;
@@ -67,9 +75,9 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
+// i didnt put any console.logs because they confuse me. I know they are a harmless command, but i still struggle where to correctly put them.
   
-  
+// Nevermind i went in to play around with them and now there are some console.logs. 
   
   
   
